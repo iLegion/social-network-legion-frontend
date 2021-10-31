15d4682c741272ec63a2190bf024e227a29162da
Yusif Zourab <wordlearningpress@gmail.com>
2021-10-31 12:42:18 +0200

    Create "Profile" page. #3 Updated "profile" page.

---
+++ components/Profile/ProfileInfo/ProfileInfo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="profile-info">
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4 d-flex justify-content-center align-items-center">
+          <img :src="user.avatar" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <div class="mb-2">
+              <h5 class="card-title d-inline-block me-2">{{ user.name }}</h5>
+              <span class="email fs-6 text-secondary">{{ user.email }}</span>
+            </div>
+
+            <ul class="list-group list-group-horizontal">
+              <li class="list-group-item text-center">
+                <div class="badge rounded-pill bg-light text-dark">100</div>
+                <div>Posts</div>
+              </li>
+              <li class="list-group-item text-center">
+                <div class="badge rounded-pill bg-light text-dark">100</div>
+                <div>Friends</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import UserModel from "~/classes/Models/User/UserModel";
+
+export default Vue.extend({
+  props: {
+    user: UserModel
+  }
+});
+</script>
+
+<style lang="scss">
+  .profile-info {
+    img {
+      width: 100px;
+      height: 100px;
+    }
+  }
+</style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -2,24 +2,7 @@
   <div class="container-fluid">
     <div class="row justify-content-center">
       <div class="col-6">
-        <div>
-          <div class="info">
-            <div class="name">{{ user.name }}</div>
-            <div class="email">{{ user.email }}</div>
-            <div class="d-flex flex-row">
-              <div class="posts-count text-center me-3">
-                <div>100</div>
-                <div>Posts</div>
-              </div>
-              <div class="friends-count text-center">
-                <div>100</div>
-                <div>Friends</div>
-              </div>
-            </div>
-          </div>
-
-          <div class="posts"></div>
-        </div>
+        <ProfileInfo :user="user"/>
       </div>
     </div>
   </div>
@@ -29,13 +12,14 @@
 import Vue from "vue";
 import { mapGetters } from "vuex";
 
+import ProfileInfo from "~/components/Profile/ProfileInfo/ProfileInfo.vue";
+
 export default Vue.extend({
+  components: {
+    ProfileInfo
+  },
   computed: {
     ...mapGetters('auth', ['user'])
   }
 });
 </script>
-
-<style scoped>
-
-</style>
