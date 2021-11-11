d8495e4921d4dc39eb41fcb790f63f9015b32812
zourabchik <44872022+zourabchik@users.noreply.github.com>
2021-11-11 19:31:27 +0300

    Created chat page

---
+++ components/Chat/ChatContent/TextField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-field-component">
+  <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Написать сообщение..."></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'TextField',
+  
+})
+</script>
+
+<style lang="scss">
+  .text-field-component {
+    .area {
+      height: 10vh;
+    }
+  }
+</style>
