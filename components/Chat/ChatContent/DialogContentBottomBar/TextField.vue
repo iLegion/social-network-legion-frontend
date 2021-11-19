7c49eb44d44218cf57f6579e8b38d3bf8fef2e93
Yusif Zourab <wordlearningpress@gmail.com>
2021-11-19 17:30:40 +0200

    Chat page. #7 Created DateService. Refactored code.

---
+++ components/Chat/ChatContent/DialogContentBottomBar/TextField.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-field-component h-100">
+    <form class="h-100">
+      <textarea class="form-control border-start-0 border-end-0 h-100"
+                id="exampleFormControlTextarea1"
+                placeholder="Написать сообщение..."></textarea>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss">
+  .text-field-component {
+    textarea {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+  }
+</style>
---
+++ components/Chat/ChatContent/DialogContentTopBar/DialogContentTopBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dialog-content-top-bar-component border-bottom">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-4 h-100">
+          <div class="fw-bold d-flex align-items-center h-100">
+            {{ dialog.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import DialogModel from "~/classes/Models/Dialog/DialogModel";
+
+export default Vue.extend({
+  props: {
+    dialog: {
+      type: Object as () => DialogModel,
+      default: () => {}
+    }
+  }
+});
+</script>
+
+<style lang="scss">
+.dialog-content-top-bar-component {
+  height: 7vh;
+}
+</style>
+
