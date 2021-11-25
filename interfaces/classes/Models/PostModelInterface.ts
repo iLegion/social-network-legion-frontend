65df5336113d987cfb035f3dde774c1c749ad9a8
zourabchik <44872022+zourabchik@users.noreply.github.com>
2021-11-25 23:12:56 +0300

    Author name in post

--- interfaces/classes/Models/PostModelInterface.ts
+++ interfaces/classes/Models/PostModelInterface.ts
@@ -1,8 +1,11 @@
+import { UserInterface } from "~/interfaces/classes/Models/User/UserModelInterface";
+
 export interface PostInterface {
   createdAt: string;
   id: number;
   likesCount: number;
   text: string;
+  author: UserInterface;
   title: string;
   updatedAt: string;
   viewsCount: number;
