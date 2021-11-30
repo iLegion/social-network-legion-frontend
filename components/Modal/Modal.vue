372f6c705f1f4033b5cbc535d92d74b45b45ee96
zourabchik <44872022+zourabchik@users.noreply.github.com>
2021-11-30 21:53:37 +0300

    Post description

---
+++ components/Modal/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="modal fade"
+    id="creditModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+            <div class="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div class="card-body">
+            <slot name="post"></slot>
+            </div>
+          </div>
+            <slot name="comment">
+                <Comment />
+            </slot> 
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import PostModel from "~/classes/Models/PostModel";
+import Comment from "~/components/Post/Comment.vue";
+
+export default Vue.extend({
+    components: {
+        Comment
+    },
+  props: {
+    post: {
+      type: Object as () => PostModel,
+      required: true
+    }
+  }
+})
+</script>
