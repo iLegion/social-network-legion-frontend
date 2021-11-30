1be2c7876444f2e00d8f84c0b00f48cba85ec595
zourabchik <44872022+zourabchik@users.noreply.github.com>
2021-11-30 21:53:37 +0300

    Post description

---
+++ components/Post/Comment.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="comment bg-light rounded">
+    <div class="comment-title h4 mb-3">Write a comment</div>
+    <div class="form-floating">
+      <textarea
+        class="form-control"
+        placeholder="Leave a comment here"
+        id="floatingTextarea2"
+        style="height: 100px"
+      >
+      </textarea>
+      <label for="floatingTextarea2">Leave your comment here</label>
+    </div>
+    <div class="d-flex justify-content-end mt-2">
+      <button type="button" class="btn btn-outline-dark">Send</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comment"
+};
+</script>
+
+<style lang="scss">
+.comment {
+  padding: 20px 30px;
+}
+</style>
--- components/Post/Post.vue
+++ components/Post/Post.vue
@@ -2,8 +2,30 @@
   <div class="card mb-3">
     <!--    <img src="..." class="card-img-top" alt="...">-->
     <div class="card-body">
+      <modal>
+        <template v-slot:post>
       <h5 class="card-title">{{ post.title }}</h5>
-      <p class="card-text">{{ post.text }}</p>
+      <p class="card-text">{{ shortDescription }}</p>
+      <div class="card-footer">
+      <PostFooter :post="post"
+                  @onAddLike="handleAddLike"
+                  @onAddView="handleAddView" />
+    </div>
+    </template>
+     </modal>
+        <h5 class="card-title">{{ post.title }}</h5>
+      <p class="card-text">{{ shortDescription }}</p>
+      <div class="d-flex justify-content-end">
+      <span
+      type="button"
+                      data-bs-toggle="modal"
+                      data-bs-target="#creditModal"
+                    >
+                      More
+                    </span>
+                    <Modal :post="post"
+                    />
+      </div>
     </div>
     <div class="card-footer">
       <PostFooter :post="post"
@@ -18,10 +40,12 @@ import Vue from 'vue'
 
 import PostModel from "~/classes/Models/PostModel";
 import PostFooter from "~/components/Post/PostFooter.vue";
+import Modal from "~/components/Modal/Modal.vue";
 
 export default Vue.extend({
   components: {
-    PostFooter
+    PostFooter,
+    Modal
   },
   props: {
     post: {
@@ -35,6 +59,11 @@ export default Vue.extend({
     },
     handleAddView(id: number): void {
       this.$emit('onAddView', id);
+    }
+  },
+  computed: {
+      shortDescription() {
+      return this.post.text.slice(0, 300) + "...";
     }
   }
 })
