a8729b555071964a3bfca494cb72e109e8054a77
Yusif Zourab <wordlearningpress@gmail.com>
2022-04-10 11:09:36 +0300

    SL-25

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,22 +5,29 @@
 <script lang="ts">
 import Vue from "vue";
 
+import Error404 from '~/components/Error/Error404.vue';
 import Error500 from '~/components/Error/Error500.vue';
 
 export default Vue.extend({
-  props: ['error'],
+  props: {
+    error: {
+      type: Object as () => { message: string, statusCode: number },
+      required: true
+    }
+  },
   layout: 'empty',
   components: {
+    Error404,
     Error500
   },
   computed: {
-    errorPage(): typeof Error500 {
-      return Error500;
+    errorPage(): typeof Error404 | typeof Error500 {
+      if (this.error.statusCode === 404) {
+        return Error404;
+      } else {
+        return Error500;
+      }
     }
   }
 });
 </script>
-
-<style scoped>
-
-</style>
