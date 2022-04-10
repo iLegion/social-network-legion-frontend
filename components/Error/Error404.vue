3aac54889bf00bc0a84acd8e3dd25d67fc3933d6
Yusif Zourab <wordlearningpress@gmail.com>
2022-04-10 11:09:36 +0300

    SL-25

--- components/Error/Error404.vue
+++ components/Error/Error404.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="error-component error-500-component">
+  <div class="error-component error-404-component">
     <div class="container">
       <div class="row">
         <div class="offset-3 col-6">
           <div class="wrapper text-center d-flex flex-column justify-content-center">
-            <div class="display-3">500</div>
-            <div class="display-5 mb-5">Interval Server Error</div>
+            <div class="display-3">404</div>
+            <div class="display-5 mb-5">Not Found</div>
             <div>
               <router-link to="/" class="btn btn-outline-dark">Return to main page.</router-link>
             </div>
